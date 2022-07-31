8c3ca592562541afbb1a6ce0580ef217a2f67e74
Jooka <mr.jooka@gmail.com>
2022-07-31 19:55:43 +0900

    client에서 field 입력안하면 실행 안함

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -6,6 +6,7 @@ export const resolvers = {
     jobs: () => Job.findAll(),
   },
   Job: {
-    company: (job) => Company.findById(job.companyId),
+    // client에서 filed 추가 안하면 resolve에서 실행 안함
+    company: (job) => (console.log('실행됨'), Company.findById(job.companyId)),
   },
 };
