e42c4549768d33fe0e8141b3da1bab1ad3d9a46b
Jooka <mr.jooka@gmail.com>
2022-08-06 04:27:01 +0900

    sql 데이터베이스

---
+++ job-board-with-sqlite/server/db.js
@@ -0,0 +1,13 @@
+import knex from 'knex';
+
+export const db = knex({
+  client: 'better-sqlite3',
+  connection: {
+    filename: './data/db.sqlite3',
+  },
+  useNullAsDefault: true,
+});
+
+db.on('query', (query) => {
+  console.log('[db] query: ', query);
+});
