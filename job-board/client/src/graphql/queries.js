eb7a4020407715a4ec843da99b614d4ed0fd3869
MrJooka <mr.jooka@gmail.com>
2022-08-05 16:50:29 +0900

    Mutation할 때 update로 query 업데이트 한번만 통신하게~

--- job-board/client/src/graphql/queries.js
+++ job-board/client/src/graphql/queries.js
@@ -1,18 +1,40 @@
-import { gql, ApolloClient, InMemoryCache } from "@apollo/client";
-import { getAccessToken } from "../auth";
+import { gql, ApolloClient, InMemoryCache } from '@apollo/client';
+import { getAccessToken } from '../auth';
 
-const GRAPHQL_URL = "http://localhost:9000/graphql";
+const GRAPHQL_URL = 'http://localhost:9000/graphql';
 
 const client = new ApolloClient({
   uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
 });
 
+const JOB_QUERY = gql`
+  query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+        description
+      }
+      description
+    }
+  }
+`;
+
 export async function createJob(input) {
   const mutation = gql`
     mutation CreateJobMutation($input: CreateJobInput!) {
       job: createJob(input: $input) {
         id
+        title
+        company {
+          id
+          name
+          description
+        }
+        description
       }
     }
   `;
@@ -26,7 +48,19 @@ export async function createJob(input) {
     data: {
       job: { id },
     },
-  } = await client.mutate({ mutation, variables, context });
+  } = await client.mutate({
+    mutation,
+    variables,
+    context,
+    update: (cache, { data: { job } }) => {
+      console.log(job);
+      cache.writeQuery({
+        query: JOB_QUERY,
+        variables: { id: job.id },
+        data: { job },
+      });
+    },
+  });
   return id;
 }
 
@@ -68,28 +102,14 @@ export async function getJobs() {
 
   const {
     data: { jobs },
-  } = await client.query({ query, fetchPolicy: "network-only" });
+  } = await client.query({ query, fetchPolicy: 'network-only' });
   return jobs;
 }
 
 export async function getJob(id) {
-  const query = gql`
-    query JobQuery($id: ID!) {
-      job(id: $id) {
-        id
-        title
-        company {
-          id
-          name
-          description
-        }
-        description
-      }
-    }
-  `;
   const variables = { id };
   const {
     data: { job },
-  } = await client.query({ query, variables });
+  } = await client.query({ query: JOB_QUERY, variables });
   return job;
 }
