8a451de04161b852fb163cef38280486ef47c3c8
Jooka <mr.jooka@gmail.com>
2022-08-06 05:01:27 +0900

    !!!중요 dataloader는 any databse에 적용 가능함.

--- job-board-with-sqlite/server/db.js
+++ job-board-with-sqlite/server/db.js
@@ -13,16 +13,16 @@ db.on('query', ({ sql, bindings }) => {
   console.log('[db]: ', sql, bindings);
 });
 
-export const companyLoader = new DataLoader(async (companyIds) => {
-  console.log('[companyLoader] companyIds: ', companyIds);
-  const companies = await db
-    .select()
-    .from('companies')
-    .whereIn('id', companyIds);
+export const createCompanyLoader = () => {
+  return new DataLoader(async (companyIds) => {
+    console.log('[companyLoader] companyIds: ', companyIds);
+    const companies = await db
+      .select()
+      .from('companies')
+      .whereIn('id', companyIds);
 
-  // companies 를 return 문에 바로 반환하지 않는 이유는
-  // companyIds의 순서와 다를수 있기 때문에 아래는 정렬해서 보내준다
-  return companyIds.map((companyId) =>
-    companies.find((company) => company.id === companyId)
-  );
-});
+    return companyIds.map((companyId) =>
+      companies.find((company) => company.id === companyId)
+    );
+  });
+};
