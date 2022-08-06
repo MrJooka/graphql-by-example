500cc1a057f1abe322981f401debcaf672b4254f
Jooka <mr.jooka@gmail.com>
2022-08-07 02:18:06 +0900

    client websocket 적용

--- chat/client/src/graphql/hooks.js
+++ chat/client/src/graphql/hooks.js
@@ -1,6 +1,10 @@
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation, useQuery, useSubscription } from '@apollo/client';
 import { getAccessToken } from '../auth';
-import { ADD_MESSAGE_MUTATION, MESSAGES_QUERY } from './queries';
+import {
+  ADD_MESSAGE_MUTATION,
+  MESSAGES_QUERY,
+  MESSAGE_ADDED_SUBSCRIPTION,
+} from './queries';
 
 export function useAddMessage() {
   const [mutate] = useMutation(ADD_MESSAGE_MUTATION);
@@ -13,14 +17,6 @@ export function useAddMessage() {
         context: {
           headers: { Authorization: 'Bearer ' + getAccessToken() },
         },
-        update: (cache, { data: { message } }) => {
-          cache.updateQuery(
-            {
-              query: MESSAGES_QUERY,
-            },
-            (oldData) => ({ messages: [...oldData.messages, message] })
-          );
-        },
       });
       return message;
     },
@@ -31,6 +27,18 @@ export function useMessages() {
   const { data } = useQuery(MESSAGES_QUERY, {
     context: {
       headers: { Authorization: 'Bearer ' + getAccessToken() },
+    },
+  });
+
+  useSubscription(MESSAGE_ADDED_SUBSCRIPTION, {
+    onSubscriptionData: ({ subscriptionData, client }) => {
+      const { message } = subscriptionData.data;
+      client.cache.updateQuery(
+        {
+          query: MESSAGES_QUERY,
+        },
+        (oldData) => ({ messages: [...oldData.messages, message] })
+      );
     },
   });
   return {
